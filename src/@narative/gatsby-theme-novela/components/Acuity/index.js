0ffcbb6eb1828e47abc11851ec5a4314a80bba53
Alex E <hello@alexentrepreneur.com>
2020-02-08 17:15:47 +0000

    Add Booking Scheduler Embed Component

---
+++ src/@narative/gatsby-theme-novela/components/Acuity/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AcuityScheduler() {
+ return (
+   <div style={{ marginBottom: '50vh', height: '85vh'}}>
+    <iframe src="https://app.acuityscheduling.com/schedule.php?owner=18993163" width="100%" height="100%" frameBorder="0"></iframe>
+    <script defer src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
+   </div>
+ );
+}
