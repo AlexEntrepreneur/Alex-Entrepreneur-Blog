13bf3521fae97cfd29b1c418d2dd252187579cbd
Alex E <hello@alexentrepreneur.com>
2020-02-08 15:27:24 +0000

    404 Page: Center Text + Add Link To Home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "@narative/gatsby-theme-novela/src/components/Layout";
-import Section from "@narative/gatsby-theme-novela/src/components/Section";
-import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
-import Headings from "@narative/gatsby-theme-novela/src/components/Headings";
+import Layout from '@narative/gatsby-theme-novela/src/components/Layout';
+import Section from '@narative/gatsby-theme-novela/src/components/Section';
+import SEO from '@narative/gatsby-theme-novela/src/components/SEO';
+import Headings from '@narative/gatsby-theme-novela/src/components/Headings';
+import Anchor from '@narative/gatsby-theme-novela/src/components/Anchor';
 
 function NotFoundPage() {
   return (
     <Layout>
       <SEO />
       <Section>
-        <div style={{ marginTop: "100px" }}>
-          <Headings.h1>404: Page Not Found</Headings.h1>
+        <div style={{ marginTop: '100px', textAlign: 'center' }}>
+          <Headings.h1 style={{ marginBottom: '50px'}}>404: Page Not Found</Headings.h1>
+          <Link to="/"><Anchor><b>Return Home</b></Anchor></Link>
         </div>
       </Section>
     </Layout>
